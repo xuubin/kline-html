7b4ec2490981707590a10b34727fe8165955ac8a
xuubin <xuubin@live.com>
2021-04-15 11:14:35 +0800

    use data from csv

--- js/option.js
+++ js/option.js
@@ -1,3 +1,19 @@
+function calculateMA(dayCount, data) {
+  var result = [];
+  for (var i = 0, len = data.length; i < len; i++) {
+    if (i < dayCount) {
+      result.push("-");
+      continue;
+    }
+    var sum = 0;
+    for (var j = 0; j < dayCount; j++) {
+      sum += data[i - j][1];
+    }
+    result.push(sum / dayCount);
+  }
+  return result;
+}
+
 function getOption(dates, data) {
   var option = {
     legend: {
